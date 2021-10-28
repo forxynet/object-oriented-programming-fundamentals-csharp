9533e38eeed6b4944e751ede315914de1c802b3d
hakanbasturk <forxynet@gmail.com>
2021-10-28 12:04:35 +0300

    CustomerRepository format updated

--- ACM.BL/CustomerRepository.cs
+++ ACM.BL/CustomerRepository.cs
@@ -7,56 +7,55 @@ using System.Threading.Tasks;
 namespace ACM.BL {
     public class CustomerRepository {
         public CustomerRepository() {
-      addressRepository = new AddressRepository();
-    }
+            addressRepository = new AddressRepository();
+        }
 
-    private AddressRepository addressRepository { get; set; }
+        private AddressRepository addressRepository { get; set; }
 
-    /// <summary>
-    /// Retrieve one customer.
-    /// </summary>
+        /// <summary>
+        /// Retrieve one customer.
+        /// </summary>
         public Customer Retrieve(int customerId) {
-      // Create the instance of the Customer class
-      // Pass in the requested id
-      Customer customer = new Customer(customerId);
+            // Create the instance of the Customer class
+            // Pass in the requested id
+            Customer customer = new Customer(customerId);
 
-      // Code that retrieves the defined customer
+            // Code that retrieves the defined customer
 
-      // Temporary hard-coded values to return 
-      // a populated customer
+            // Temporary hard-coded values to return 
+            // a populated customer
             if (customerId == 1) {
-        customer.EmailAddress = "fbaggins@hobbiton.me";
-        customer.FirstName = "Frodo";
-        customer.LastName = "Baggins";
-        customer.AddressList = addressRepository.RetrieveByCustomerId(customerId).
-                                    ToList();
-      }
-      return customer;
-    }
+                customer.EmailAddress = "fbaggins@hobbiton.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+                customer.AddressList = addressRepository.RetrieveByCustomerId(customerId).
+                                            ToList();
+            }
+            return customer;
+        }
 
-    /// <summary>
-    /// Saves the current customer.
-    /// </summary>
-    /// <returns></returns>
+        /// <summary>
+        /// Saves the current customer.
+        /// </summary>
+        /// <returns></returns>
         public bool Save(Customer customer) {
-      var success = true;
+            var success = true;
 
             if (customer.HasChanges) {
                 if (customer.IsValid) {
                     if (customer.IsNew) {
-            // Call an Insert Stored Procedure
+                        // Call an Insert Stored Procedure
 
-          }
+                    }
                     else {
-            // Call an Update Stored Procedure
-          }
-        }
+                        // Call an Update Stored Procedure
+                    }
+                }
                 else {
-          success = false;
+                    success = false;
+                }
+            }
+            return success;
         }
-      }
-      return success;
     }
-
-  }
 }
