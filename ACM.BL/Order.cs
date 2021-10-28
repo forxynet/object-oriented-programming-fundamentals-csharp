89a3cc615e3059feb093c580016ff26b8db559ba
hakanbasturk <forxynet@gmail.com>
2021-10-28 12:05:45 +0300

    Order format updated

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -6,34 +6,34 @@ namespace ACM.BL {
     public class Order : EntityBase, ILoggable {
         public Order() : this(0) {
 
-    }
+        }
         public Order(int orderId) {
-      OrderId = orderId;
-      OrderItems = new List<OrderItem>();
-    }
+            OrderId = orderId;
+            OrderItems = new List<OrderItem>();
+        }
 
-    public int CustomerId { get; set; }
-    public DateTimeOffset? OrderDate { get; set; }
-    public int OrderId { get; private set; }
-    public List<OrderItem> OrderItems { get; set; }
-    public int ShippingAddressId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public int ShippingAddressId { get; set; }
 
-    public string Log() =>
-        $"{OrderId}: Date: {this.OrderDate.Value.Date} Status: {this.EntityState.ToString()}";
+        public string Log() =>
+            $"{OrderId}: Date: {this.OrderDate.Value.Date} Status: {this.EntityState.ToString()}";
 
-    public override string ToString() =>
-          $"{OrderDate.Value.Date} ({OrderId})";
+        public override string ToString() =>
+              $"{OrderDate.Value.Date} ({OrderId})";
 
-    /// <summary>
-    /// Validates the order data.
-    /// </summary>
-    /// <returns></returns>
+        /// <summary>
+        /// Validates the order data.
+        /// </summary>
+        /// <returns></returns>
         public override bool Validate() {
-      var isValid = true;
+            var isValid = true;
 
-      if (OrderDate == null) isValid = false;
+            if (OrderDate == null) isValid = false;
 
-      return isValid;
+            return isValid;
+        }
     }
-  }
 }
