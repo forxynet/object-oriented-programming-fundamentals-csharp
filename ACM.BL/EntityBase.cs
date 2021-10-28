ef6a05a9e870ebdec6251d74e08a404e8ee899ba
hakanbasturk <forxynet@gmail.com>
2021-10-28 12:05:11 +0300

    EntityBase format updated

--- ACM.BL/EntityBase.cs
+++ ACM.BL/EntityBase.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 
 namespace ACM.BL {
     public enum EntityStateOption {
-    Active,
-    Deleted
-  }
+        Active,
+        Deleted
+    }
 
     public abstract class EntityBase {
-    public EntityStateOption EntityState { get; set; }
-    public bool HasChanges { get; set; }
-    public bool IsNew { get; private set; }
-    public bool IsValid => Validate();
+        public EntityStateOption EntityState { get; set; }
+        public bool HasChanges { get; set; }
+        public bool IsNew { get; private set; }
+        public bool IsValid => Validate();
 
-    public abstract bool Validate();
-
-  }
+        public abstract bool Validate();
+    }
 }
